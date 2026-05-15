b97484392c57ec023afc1a66346f44afbd2a93ac
Sumit Kumar <sharansumitkumar@gmail.com>
2026-05-15 13:44:35 +0530

    code updated

---
+++ android/build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+
+group = 'com.aaassseee.screen_brightness_android'
+version = '1.0-SNAPSHOT'
+
+repositories {
+        google()
+        mavenCentral()
+    }
+
+android {
+    namespace = 'com.aaassseee.screen_brightness_android'
+
+    compileSdk=37
+
+    compileOptions {
+        sourceCompatibility JavaVersion.VERSION_21
+        targetCompatibility JavaVersion.VERSION_21
+    }
+
+    kotlinOptions {
+        jvmTarget = '21'
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("src/main/kotlin")
+        }
+    }
+
+    defaultConfig {
+        minSdk 24
+    }
+}
